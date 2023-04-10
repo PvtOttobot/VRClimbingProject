66a19e7e3a3a5fd5db3b38c517acceffaff1ed96
Otto <danielottojackpetersen2@gmail.com>
2023-04-10 15:44:20 +0100

    fix: removed previously tracked android build files

--- android/build/build/intermediates/assets/debug/level.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var interface: OpenXRInterface = XRServer.find_interface("OpenXR")
-	if interface and interface.is_initialized():
-		get_viewport().use_xr = true
